276e64e623a03fab42a9f76b6e1177c2152c4cd7
hams94 <hamidouyalagueya94@gmail.com>
2021-10-20 10:56:07 +0000

    first commit

---
+++ application/views/includes/sidebar_menu.php
@@ -0,0 +1,72 @@
+ <!-- sidebar menu -->
+ <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a  href="<?php echo site_url('welcome/') ?>"><i class="fa fa-home"></i> Accueil <span class="fa fa-chevron-right"></span></a>
+                    
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Utilisateurs <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Ajouter un utilisateur</a></li>
+                      <li><a href="form_advanced.html">Liste des utilisateurs</a></li>
+                      <li><a href="form_advanced.html">Gestion des privilèges</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-desktop"></i> Universites <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="general_elements.html">Ajouter une Université</a></li>
+                      <li><a href="media_gallery.html">Liste des Universités</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-table"></i> Chefs de Départements <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="tables.html">Ajouter </a></li>
+                      <li><a href="tables_dynamic.html">Afficher la liste</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-bar-chart-o"></i> Etudiants<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="chartjs.html">Ajouter un Etudiant</a></li>
+                      <li><a href="chartjs2.html">Liste des Etudiants</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-clone"></i>Facultés <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">Ajouter une Faculté</a></li>
+                      <li><a href="fixed_footer.html">Liste des Facultés</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-clone"></i>Departements <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">Ajouter un département</a></li>
+                      <li><a href="fixed_footer.html">Liste des Départements</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-clone"></i>Licences <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">Ajouter une Licence</a></li>
+                      <li><a href="fixed_footer.html">Liste des Licences</a></li>
+                      <li><a href="fixed_footer.html">Programmes </a></li>
+                      <li><a href="fixed_footer.html">Emploies du Temps </a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-clone"></i>Examens <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="fixed_sidebar.html">Ajouter un Examen</a></li>
+                      <li><a href="fixed_footer.html">Liste des Examens</a></li>
+                      <li><a href="fixed_footer.html">Notes </a></li>
+                      <li><a href="fixed_footer.html">Resultats </a></li>
+                      
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              
+
+            </div>
+            <!-- /sidebar menu -->
